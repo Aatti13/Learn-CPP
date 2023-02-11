b30cc554d38008cafdd67eeacd25de17a0eca0e3
Aatti13 <86865554+Aatti13@users.noreply.github.com>
2023-02-11 22:56:52 +0530

    Create multiplicationtable.cpp

---
+++ Solutions/multiplicationtable.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int x;
+	
+	cout<<"Enter number";
+	
+	cin>>x;
+	
+	for (int i=1;i<=12;i++){
+		cout<<x<<"x"<<i<<"="<<x*i<<endl;
+	}
+	
+	
+	return 0;
+}
